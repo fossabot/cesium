16b9195a83310fa82fa9cd23e7ea5580ecf982b1
Michael P. Gerlek <mpg@flaxen.com>
2015-11-17 23:03:30 -0500

    updated ant version

--- docker/build.sh
+++ docker/build.sh
@@ -10,7 +10,7 @@ yum -y install nodejs npm
 #
 # Ant
 #
-curl -L https://www.apache.org/dist/ant/binaries/apache-ant-1.9.5-bin.tar.gz -o /tmp/ant.tar.gz
+curl -L https://www.apache.org/dist/ant/binaries/apache-ant-1.9.6-bin.tar.gz -o /tmp/ant.tar.gz
 tar xvf /tmp/ant.tar.gz -C /tmp
 rm -rf /tmp/ant.tar.gz
 
@@ -20,7 +20,7 @@ rm -rf /tmp/ant.tar.gz
 curl -L https://github.com/radiantbluetechnologies/rialto-cesium/archive/${RIALTO_BRANCH}_rialto.zip -o /tmp/cesium.zip
 unzip -o -d /tmp /tmp/cesium.zip
 mv /tmp/rialto-cesium-${RIALTO_BRANCH}_rialto /tmp/cesium
-/tmp/apache-ant-1.9.5/bin/ant release -buildfile /tmp/cesium
+/tmp/apache-ant-1.9.6/bin/ant release -buildfile /tmp/cesium
 mkdir -p /opt/cesium-build
 cp -r /tmp/cesium/* /opt/cesium-build/
 
@@ -29,6 +29,6 @@ cp -r /tmp/cesium/* /opt/cesium-build/
 #
 yum clean all
 rm -rf \
-    /tmp/apache-ant-1.9.5 \
+    /tmp/apache-ant-1.9.6 \
     /tmp/cesium/ \
     /tmp/cesium.zip
