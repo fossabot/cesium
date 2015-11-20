7097aed5944d232f93a9b4e6940d9f04a2502853
Michael P. Gerlek <mpg@flaxen.com>
2015-11-20 09:46:49 -0500

    name fixes, cleanups

--- docker/build.sh
+++ docker/build.sh
@@ -17,12 +17,9 @@ rm -rf /tmp/ant.tar.gz
 #
 # Rialto's Cesium
 #
-#curl -L https://github.com/radiantbluetechnologies/rialto-cesium/archive/${RIALTO_BRANCH}_rialto.zip -o /tmp/cesium.zip
-#unzip -o -d /tmp /tmp/cesium.zip
-#mv /tmp/rialto-cesium-${RIALTO_BRANCH}_rialto /tmp/cesium
-/tmp/apache-ant-1.9.6/bin/ant release -buildfile /tmp/rialto-cesium
+/tmp/apache-ant-1.9.6/bin/ant release -buildfile /tmp/cesium
 mkdir -p /opt/cesium-build
-cp -r /tmp/rialto-cesium/* /opt/cesium-build/
+cp -r /tmp/cesium/* /opt/cesium-build/
 
 #
 # cleanup
